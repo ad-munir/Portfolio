941111004129146ff8125a2e2f344d3e8471fb4b
ad-munir <mounir.ayad@etu.uae.ac.ma>
2023-08-19 23:49:50 +0100

    + myskills style

--- src/components/MySkills/MySkills.jsx
+++ src/components/MySkills/MySkills.jsx
@@ -1,36 +1,64 @@
-import './MySkills.css'
+import './MySkills.css';
+
 const MySkills = () => {
     return (
         <section className="my-skills">
-        <h2 className="section-title">My Skills</h2>
-        <div className="skills-container">
-            <div className="skill-card">
-                <h3 className="skill-title">Backend Development</h3>
-                <ul className="technology-list">
-                    <li>Java EE</li>
-                    <li>Php</li>
-                    <li>Laravel</li>
-                </ul>
+            <h2 className="section-title">My Skills</h2>
+            <div className="skills-container">
+                <div className="skill-card">
+                    <h3 className="skill-title">Backend Development</h3>
+                    <ul className="technology-list">
+                        <li>
+                            <img src="https://w7.pngwing.com/pngs/279/249/png-transparent-java-logo-programming-language-java-plum-miscellaneous-text-orange-thumbnail.png" alt="Java" />
+                            Java EE
+                        </li>
+                        <li>
+                            <img src="path-to-php-logo.png" alt="PHP" />
+                            Php
+                        </li>
+                        <li>
+                            <img src="path-to-laravel-logo.png" alt="Laravel" />
+                            Laravel
+                        </li>
+                    </ul>
+                </div>
+                <div className="skill-card">
+                    <h3 className="skill-title">Frontend Development</h3>
+                    <ul className="technology-list">
+                        <li>
+                            <img src="path-to-html-logo.png" alt="HTML" />
+                            HTML
+                        </li>
+                        <li>
+                            <img src="path-to-css-logo.png" alt="CSS" />
+                            CSS
+                        </li>
+                        <li>
+                            <img src="path-to-js-logo.png" alt="JavaScript" />
+                            JavaScript
+                        </li>
+                        <li>
+                            <img src="path-to-react-logo.png" alt="React" />
+                            React
+                        </li>
+                    </ul>
+                </div>
+                <div className="skill-card">
+                    <h3 className="skill-title">Databases</h3>
+                    <ul className="technology-list">
+                        <li>
+                            <img src="path-to-mysql-logo.png" alt="MySQL" />
+                            MySQL
+                        </li>
+                        <li>
+                            <img src="path-to-mongodb-logo.png" alt="MongoDB" />
+                            MongoDB
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className="skill-card">
-                <h3 className="skill-title">Frontend Development</h3>
-                <ul className="technology-list">
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                </ul>
-            </div>
-            <div className="skill-card">
-                <h3 className="skill-title">Databases</h3>
-                <ul className="technology-list">
-                    <li>MySQL</li>
-                    <li>MongoDB</li>
-                </ul>
-            </div>
-        </div>
-    </section>
-    )
+        </section>
+    );
 }
 
-export default MySkills
+export default MySkills;
